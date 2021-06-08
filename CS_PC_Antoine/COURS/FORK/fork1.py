5f3dbaf29e4bfb4277f62f4e4fd10a973603ea16
Adrien <72149879+AdrienHrdz@users.noreply.github.com>
2021-06-08 15:50:01 +0200

    Delete CS_PC_Antoine directory

--- CS_PC_Antoine/COURS/FORK/fork1.py
+++
@@ -1,4 +0,0 @@
-import os
-print("PID du PERE : ", os.getpid())
-pid= os.fork()
-print("PID du FILS : ", pid)
--- CS_PC_Antoine/COURS/FORK/fork3.py
+++
@@ -1,6 +0,0 @@
-import os
-print("Je suis le processus PERE = ", os.getpid())
-pid1 = os.fork()
-print(pid1)
-pid2 = os.fork()
-print(pid2)
